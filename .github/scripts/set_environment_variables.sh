1444e7a568c65da230e4650ce6785a08e25335d6
Håkon Hægland <hakon.hagland@gmail.com>
2021-06-28 15:13:42 +0200

    Mon Jun 28 15:13:42 2021

--- .github/scripts/set_environment_variables.sh
+++ .github/scripts/set_environment_variables.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-echo "PERL_NAME=$PERL_NAME" >> $GITHUB_ENV
-echo "PERL_DIR=$PERL_DIR" >> $GITHUB_ENV
 if [[ -n $PERL_DIR ]] ; then
+    echo "PERL_NAME=$PERL_NAME" >> $GITHUB_ENV
+    echo "PERL_DIR=$PERL_DIR" >> $GITHUB_ENV
     export PATH=$PERL_DIR/bin:"$PATH"
 fi
-echo "GSL_NAME=$GSL_NAME" >> $GITHUB_ENV
-echo "GSL_DIR=$GSL_DIR" >> $GITHUB_ENV
 if [[ -n $GSL_DIR ]] ; then
+    # autoconf needs an absolute prefix to install GSL, replace tilde with $HOME
+    GSL_DIR="${GSL_DIR/#\~/$HOME}"
+    echo "GSL_NAME=$GSL_NAME" >> $GITHUB_ENV
+    echo "GSL_DIR=$GSL_DIR" >> $GITHUB_ENV
     export LD_LIBRARY_PATH=$GSL_DIR/lib
     export PATH=$GSL_DIR/bin:"$PATH"
     export PKG_CONFIG_PATH="$GSL_DIR"/lib/pkgconfig
