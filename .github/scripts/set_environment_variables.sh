93249cfb07fd0d82fe6acd4befcf034a2f6f6119
Håkon Hægland <hakon.hagland@gmail.com>
2021-06-28 15:23:29 +0200

    Mon Jun 28 15:23:29 2021

--- .github/scripts/set_environment_variables.sh
+++ .github/scripts/set_environment_variables.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 if [[ -n $PERL_DIR ]] ; then
+    PERL_DIR="${PERL_DIR/#\~/$HOME}"
     echo "PERL_NAME=$PERL_NAME" >> $GITHUB_ENV
     echo "PERL_DIR=$PERL_DIR" >> $GITHUB_ENV
     export PATH=$PERL_DIR/bin:"$PATH"
