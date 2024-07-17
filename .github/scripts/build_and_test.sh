f274bdfe50364d81bb306c4fd9825fbb4ed78271
Håkon Hægland <hakon.hagland@gmail.com>
2024-07-17 12:11:46 +0200

    Use Alien::GSL::Shared instead of Alien::GSL (#32)
    * Use Alien::GSL::Shared
    
    * Also install dependencies

--- .github/scripts/build_and_test.sh
+++ .github/scripts/build_and_test.sh
@@ -2,7 +2,15 @@
 
 cpanm -vn Net::SSLeay
 cpanm -n Alien::Build
-cpanm Alien::GSL
+git clone https://github.com/hakonhagland/Alien-GSL-Shared.git
+cd Alien-GSL-Shared
+cpanm -n -v --installdeps .
+perl Makefile.PL
+make
+#make test  # Tests are failing currently
+make install
+cd ..
+#cpanm Alien::GSL
 cpanm Module::Build
 mkdir -p xs
 perl Build.PL
