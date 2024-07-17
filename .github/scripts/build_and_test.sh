62692bb8dd93463dcac9c12a54e9ab3089f144db
Håkon Hægland <hakon.hagland@gmail.com>
2024-07-17 15:15:13 +0200

    Use Math::GSL::Alien instead of Alien::GSL (#33)
    * Use Math::GSL::Alien
    
    * Rebuild
    
    * Fixed Math::GSL::Alien on CPAN
    
    Retry with new version
    
    * Trigger workflow
    
    Check if it will use version 1.03 of Math::GSL::Alien
    
    * Use explicitly version 1.03

--- .github/scripts/build_and_test.sh
+++ .github/scripts/build_and_test.sh
@@ -2,16 +2,8 @@
 
 cpanm -vn Net::SSLeay
 cpanm -n Alien::Build
-git clone https://github.com/hakonhagland/Alien-GSL-Shared.git
-cd Alien-GSL-Shared
-cpanm -n -v --installdeps .
-perl Makefile.PL
-make
-#make test  # Tests are failing currently
-make install
-cd ..
-#cpanm Alien::GSL
-cpanm Module::Build
+cpanm -vn Math::GSL::Alien@1.03
+cpanm -n Module::Build
 mkdir -p xs
 perl Build.PL
 ./Build installdeps --cpan_client cpanm
