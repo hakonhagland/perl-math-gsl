96b0e3079dbdd7e645503a1d2205089d70902520
Håkon Hægland <hakon.hagland@gmail.com>
2024-07-17 14:51:31 +0200

    Trigger workflow
    Check if it will use version 1.03 of Math::GSL::Alien

--- .github/scripts/build_and_test.sh
+++ .github/scripts/build_and_test.sh
@@ -3,7 +3,7 @@
 cpanm -vn Net::SSLeay
 cpanm -n Alien::Build
 cpanm -vn Math::GSL::Alien
-cpanm Module::Build
+cpanm -n Module::Build
 mkdir -p xs
 perl Build.PL
 ./Build installdeps --cpan_client cpanm
